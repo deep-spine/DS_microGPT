ff772443fd8c621c8893b3e6fdce95217eaeac58
kandarpa02 <kandarpaexe@gmail.com>
2025-07-06 01:32:59 +0530

    fix issues

--- microGPT/decoder/embed/embedding.py
+++ microGPT/decoder/embed/embedding.py
@@ -21,24 +21,28 @@ def pos_encoding(seq_len, d_model):
     out = out = out.reshape(seq_len, d_model)
     return out
 
-pos_encoding = jax.jit(pos_encoding, static_argnames=('d_model', 'seq_len'))
-
 
 def _embed(params, token_idx):
     emb = params["embedding_table"][token_idx]
     d_model = params["embedding_table"].shape[1]
     return emb * jnp.sqrt(d_model)
 
+
+
 def word_embedding(params, tokens):
-    emb_table = params["embedding_table"] 
+    emb_table = params["embedding_table"]
     d_model = emb_table.shape[1]
 
     embeddings = emb_table[tokens] * jnp.sqrt(d_model)
 
     seq_len = tokens.shape[1]
-    pos = pos_encoding(seq_len, d_model)  # (seq_len, d_model)
-    pos = jnp.expand_dims(pos, axis=0)    # (1, seq_len, d_model)
 
-    return embeddings + pos 
+    MAX_SEQ_LEN = 2048 
+
+    full_pos = pos_encoding(MAX_SEQ_LEN, d_model) 
+    pos = full_pos[:seq_len]                      
+    pos = jnp.expand_dims(pos, axis=0)     
+
+    return embeddings + pos
 
 
