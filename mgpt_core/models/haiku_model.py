1615331b91c52bf2eff9e5d54e0f88270069d2a7
kandarpa02 <kandarpaexe@gmail.com>
2025-07-12 00:21:14 +0530

    defined 2 models in haiku and torch

---
+++ mgpt_core/models/haiku_model.py
@@ -0,0 +1,124 @@
+# ==============================================================================
+#  Project: ds_microGPT
+#  Author: DeepSpine (2025–present)
+#  License: MIT
+#
+#  A compact, distilled GPT architecture written in JAX/Haiku.
+#  Designed for high-output, low-weight inference, TensorFlow export,
+#  and real-world fine-tuned deployment.
+#
+#  Copyright (c) 2025–present DeepSpine.
+#  Licensed under the MIT License. See LICENSE for details.
+# ==============================================================================
+
+
+from mgpt_core.layers.attention import MhAttention
+from mgpt_core.layers.ffn import FFN_Nami
+import haiku as hk
+import jax
+import jax.numpy as jnp
+
+class Transformer(hk.Module):
+    def __init__(self, dim:int, heads:int, name = "transformer"):
+        super().__init__(name=name)
+        self.LN1 = hk.LayerNorm(axis=-1, create_scale=True, create_offset=True)
+        self.LN2 = hk.LayerNorm(axis=-1, create_scale=True, create_offset=True)
+        self.Attn = MhAttention(dim, heads)
+        self.FFN = FFN_Nami(dim)
+    
+    def __call__(self, x):
+        x = x + self.Attn(self.LN1(x))
+        x = x + self.FFN(self.LN2(x))
+
+        return x
+
+
+class Hyuga_neuro05(hk.Module):
+    def __init__(
+        self,
+        vocab_size: int,
+        name="hyuga-neuro05"
+    ):
+        super().__init__(name=name)
+        self.vocab_size = vocab_size
+        self.dim = 512
+        self.heads = 16
+        self.num_layers = 128
+
+        self.embedding_weights = hk.get_parameter(
+            "embed", shape=[vocab_size, self.dim], init=hk.initializers.TruncatedNormal(stddev=0.02)
+        )
+
+        self.blocks = [
+            Transformer(self.dim, self.heads, name=f"transformer_{i}") for i in range(self.num_layers)
+        ]
+
+    def __call__(self, x: jnp.ndarray) -> jnp.ndarray:
+
+        x = jnp.take(self.embedding_weights, x, axis=0) 
+
+        for block in self.blocks:
+            x = block(x)
+
+        # output projection: tie weights by using embedding matrix transposed
+        logits = x @ self.embedding_weights.T
+        return logits
+
+
+class Hyuga_echo(hk.Module):
+    def __init__(
+        self,
+        vocab_size: int,
+        name="hyuga-echo"
+    ):
+        super().__init__(name=name)
+        self.vocab_size = vocab_size
+        self.dim = 1024
+        self.heads = 32
+        self.num_layers = 36
+
+        self.embedding_weights = hk.get_parameter(
+            "embed", shape=[vocab_size, self.dim], init=hk.initializers.TruncatedNormal(stddev=0.02)
+        )
+
+        self.blocks = [
+            Transformer(self.dim, self.heads, name=f"transformer_{i}") for i in range(self.num_layers)
+        ]
+
+    def __call__(self, x: jnp.ndarray) -> jnp.ndarray:
+
+        x = jnp.take(self.embedding_weights, x, axis=0) 
+
+        for block in self.blocks:
+            x = block(x)
+
+        # output projection: tie weights by using embedding matrix transposed
+        logits = x @ self.embedding_weights.T
+        return logits
+
+
+# from mgpt_core.seed import KEYS
+# k1, k2, k3 = KEYS(42, 3)
+# def f(x):
+#     t = Hyuga(
+#         vocab_size=50304
+#         )
+#     return t(x)
+
+# net = hk.transform(f)
+# x = jax.random.randint(k1, (1, 3072), 1, 1000)
+
+# param = net.init(k2, x)
+
+# # out = net.apply(param, k3, x)
+
+# print(x.shape)
+
+
+
+# def count_params(params):
+#     return sum(jnp.size(x) for x in jax.tree_util.tree_leaves(params))
+
+# print(count_params(param)*1e-6) 
+
+# del param, x, net
---
+++ mgpt_core/models/torch_model.py
@@ -0,0 +1,155 @@
+# ==============================================================================
+#  Project: ds_microGPT
+#  Author: DeepSpine (2025–present)
+#  License: MIT
+#
+#  A compact, distilled GPT architecture written in JAX/Haiku.
+#  Designed for high-output, low-weight inference, TensorFlow export,
+#  and real-world fine-tuned deployment.
+#
+#  Copyright (c) 2025–present DeepSpine.
+#  Licensed under the MIT License. See LICENSE for details.
+# ==============================================================================
+
+
+import torch
+import torch.nn as nn
+from nami.Torch import Nami 
+from mgpt_core.layers.rotary_positional_embedding import get_freqs_t, RoPE_t
+
+
+def split_heads(x, n_heads):
+    batch, seq, dim = x.shape
+    n_dim = dim // n_heads
+    x = x.view(batch, seq, n_heads, n_dim).transpose(1, 2)
+    return x
+
+def merge_heads(x):
+    batch, n_heads, seq, n_dim = x.shape
+    x = x.transpose(1, 2).contiguous().view(batch, seq, n_heads * n_dim)
+    return x
+
+
+class MhAttention(nn.Module):
+    def __init__(self, d_model, n_heads, mask=False, device = torch.device('cuda')):
+        super().__init__()
+        self.d_model = d_model
+        self.n_heads = n_heads
+        self.mask = mask
+        self.device = device
+
+        self.qw = nn.Parameter(torch.randn(d_model, d_model))
+        self.kw = nn.Parameter(torch.randn(d_model, d_model))
+        self.vw = nn.Parameter(torch.randn(d_model, d_model))
+        self.ow = nn.Parameter(torch.randn(d_model, d_model))
+
+    def forward(self, x):
+        B, S, D = x.shape
+        n_dim = D // self.n_heads
+
+        Q = x @ self.qw
+        K = x @ self.kw
+        V = x @ self.vw
+
+        Q = Q.view(B, S, self.n_heads, n_dim)
+        K = K.view(B, S, self.n_heads, n_dim)
+
+        freqs = get_freqs_t(S, n_dim, device=self.device)
+        Q = RoPE_t(Q, freqs)
+        K = RoPE_t(K, freqs)
+
+        Q = Q.permute(0, 2, 1, 3)  # (B, H, S, D)
+        K = K.permute(0, 2, 1, 3)
+        V = split_heads(V, self.n_heads)
+
+        scores = Q @ K.transpose(-1, -2) / torch.sqrt(self.d_model)
+        scores = scores.float()
+
+        if self.mask:
+            mask = torch.tril(torch.ones(S, S, dtype=torch.float32, device=x.device))
+            scores = scores.masked_fill(mask[None, None, :, :] == 0, -1e10)
+
+        weights = torch.nn.functional.softmax(scores, dim=-1)
+        attn = weights @ V
+        out = merge_heads(attn) @ self.ow
+        return out
+
+
+class FFN_Nami(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.fc = nn.Linear(dim, dim * 4)
+        self.gate = nn.Linear(dim, dim * 4)
+        self.proj = nn.Linear(dim * 4, dim)
+        self.nami = Nami()
+
+    def forward(self, x):
+        _x = self.fc(x)
+        x_gate = self.nami(self.gate(x))
+        return self.proj(_x * x_gate)
+
+
+class Transformer(nn.Module):
+    def __init__(self, dim, heads, mask=False):
+        super().__init__()
+        self.ln1 = nn.LayerNorm(dim)
+        self.ln2 = nn.LayerNorm(dim)
+        self.attn = MhAttention(dim, heads, mask=mask)
+        self.ffn = FFN_Nami(dim)
+
+    def forward(self, x):
+        x = x + self.attn(self.ln1(x))
+        x = x + self.ffn(self.ln2(x))
+        return x
+
+
+class Hyuga_neuro05(nn.Module):
+    def __init__(self, vocab_size, mask=False):
+        super().__init__()
+        self.dim = 512
+        self.heads = 16
+        self.num_layers = 128
+        self.vocab_size = vocab_size
+
+        self.embed = nn.Embedding(vocab_size, self.dim)
+        self.transformers = nn.ModuleList([
+            Transformer(self.dim, self.heads, mask=mask) for _ in range(self.num_layers)
+        ])
+
+        self.out_proj = lambda x: x @ self.embed.weight.T
+
+    def forward(self, x):
+        x = self.embed(x)
+        for block in self.transformers:
+            x = block(x)
+        return self.out_proj(x)
+
+
+class Hyuga_echo(nn.Module):
+    def __init__(self, vocab_size, mask=False):
+        super().__init__()
+        self.dim = 1024
+        self.heads = 32
+        self.num_layers = 36
+        self.vocab_size = vocab_size
+
+        self.embed = nn.Embedding(vocab_size, self.dim)
+        self.transformers = nn.ModuleList([
+            Transformer(self.dim, self.heads, mask=mask) for _ in range(self.num_layers)
+        ])
+
+        self.out_proj = lambda x: x @ self.embed.weight.T
+
+    def forward(self, x):
+        x = self.embed(x)
+        for block in self.transformers:
+            x = block(x)
+        return self.out_proj(x)
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+model = torch.compile(Hyuga_neuro05(vocab_size=32000))
+print(model)
+print(f"Total trainable parameters: {count_parameters(model):,}")
+
